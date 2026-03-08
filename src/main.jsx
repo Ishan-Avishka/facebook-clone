e4c86ab2b76c11e94a5f1ab884f4153d67ebb55e
Ishan Avishka De Silva <avishkaishan637@gmail.com>
2026-03-08 22:01:46 +0530

    Make Changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { PostProvider } from './context/PostContext';
+import App from './App';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <AuthProvider>
+      <PostProvider>
+        <App />
+      </PostProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
